0ce8dfe8ece0cfe182f072794fc169a78ca6a043
Kaartikeya Panjwani <52821246+kaartikeya15@users.noreply.github.com>
2023-11-18 23:33:09 -0500

    Add files via upload

---
+++ Week 7/2. Minimum_Number_of_Arrows_to_Burst_Balloons.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points = sorted(points)
+        min_right = points[0][1]
+        ans = 1
+        if len(points) > 1:
+            for b in points[1:]:
+                if min_right >= b[0]:
+                    min_right = min(min_right, b[1])
+                else:
+                    ans+=1
+                    min_right = b[1]
+        return ans
